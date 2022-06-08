c8d0c7b7b5d56d4354008d49e1b919727fbc0582
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-08 19:39:45 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked Submit');                    // if its submit, then alert you clicked
             } else {
                 let gameType = this.getAttribute('data-type');  //else you clicked, either, multiply, divide, subtract, or addition 
-                alert(`You clicked ${gameType}`);                          
+                runGame(gameType);                         
             }
         });
     }
+    runGame('addition');
 });
 
 /* This is a docstring, if you hover over the function call it will show this description of what the function does*/ 
@@ -22,10 +23,17 @@ document.addEventListener('DOMContentLoaded', function() {
  * The main game loop, called when the script is loaded and
  * after the users answer has been processed.
  */
-function runGame(){
+function runGame(gameType){
     //Generate a random number between 1 and 25, 0 would create problems for division.
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if(gameType === 'addition'){
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer(){
@@ -44,19 +52,23 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
 function displaySubtractQuestion(){
-
+    
 }
 
 function displayMultiplyQuestion(){
-
+    
 }
 
 function displayDivisionQuestion(){
+    
 
 }
 
