d1da2789e1ea5dd594928eddd7a20f3bdaf742b2
gfpkelly1986 <gfpkelly@gmail.com>
2022-06-08 17:57:32 +0000

    Add code to generate random numbers for game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,28 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     let buttons = document.getElementsByTagName('button'); //grab all the buttons and store them in a variable
-    
+
     for(let button of buttons){  // loop through the HTML collection and on each iteration attatch a click event listener
         button.addEventListener('click', function(){
             if(this.getAttribute('data-type') === 'submit'){  // check the value of the datatype attribute on the element that triggered the event
                 alert('You clicked Submit');                    // if its submit, then alert you clicked
             } else {
-                let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);              //else you clicked, either, multiply, divide, subtract, or addition 
+                let gameType = this.getAttribute('data-type');  //else you clicked, either, multiply, divide, subtract, or addition 
+                alert(`You clicked ${gameType}`);                          
             }
         });
     }
 });
 
-
+/* This is a docstring, if you hover over the function call it will show this description of what the function does*/ 
+/**
+ * The main game loop, called when the script is loaded and
+ * after the users answer has been processed.
+ */
 function runGame(){
-
+    //Generate a random number between 1 and 25, 0 would create problems for division.
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
 
 function checkAnswer(){
